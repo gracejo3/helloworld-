be95e4e3eb68095be70e88f1b979e17b4f585338
johngrace <johngrace@johns-mbp.att.net>
2016-01-06 11:14:23 -0600

    test test

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String h1 = new String("Hello World");
+		System.out.println(h1);
+				
+	
+
+	}
+
+}
